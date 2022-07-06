3bf0e36f0ef2d1e14d3e0ddb65dc04956b570056
Larissa Vieira <lv23@nyu.edu>
2022-07-06 00:30:21 -0400

    Added Image model to enable users to upload multiple images to project

--- crafter_tracker/models.py
+++ crafter_tracker/models.py
@@ -5,12 +5,23 @@ class Project(models.Model):
     name = models.CharField(max_length=50)
     category = models.CharField(max_length=50)
     complete = models.BooleanField()
-    images = models.ImageField(upload_to ='uploads/')
     materials = models.CharField(max_length=50)
     resources = models.CharField(max_length=50)
     start_date = models.DateField()
     end_date = models.DateField()
     notes = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    name = models.CharField(max_length=255)
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='images',
+    )
+    images = models.ImageField(upload_to ='images/')
 
     def __str__(self):
         return self.name
