38d541050f85535ab74cceed5737533359bf92c9
Larissa Vieira <lv23@nyu.edu>
2022-07-06 00:06:18 -0400

    Basic project model created and tested

--- crafter_tracker/models.py
+++ crafter_tracker/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    complete = models.BooleanField()
+    images = models.ImageField(upload_to ='uploads/')
+    materials = models.CharField(max_length=50)
+    resources = models.CharField(max_length=50)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    notes = models.TextField()
+
+    def __str__(self):
+        return self.name
