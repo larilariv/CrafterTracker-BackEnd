dd83c4cda53cc702ee0cbcee8ef26dc175903456
Larissa Vieira <lv23@nyu.edu>
2022-07-09 10:54:40 -0400

    Added Material model to enable users to link materials to project. Set max length of all fields to max

--- crafter_tracker/models.py
+++ crafter_tracker/models.py
@@ -2,16 +2,44 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    name = models.CharField(max_length=50)
-    category = models.CharField(max_length=50)
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
     complete = models.BooleanField()
-    materials = models.CharField(max_length=50)
     start_date = models.DateField()
     end_date = models.DateField()
     notes = models.TextField()
 
     def __str__(self):
         return self.name
+
+# class ProjectCategory(models.Model):
+#     project = models.ForeignKey(
+#         Project,
+#         on_delete=models.CASCADE,
+#         related_name='categories',
+#     )
+#     cross_stitch = models.BooleanField()
+#     crocheting = models.BooleanField()
+#     embroidery = models.BooleanField()
+#     felting = models.BooleanField()
+#     knitting = models.BooleanField()
+#     lace_making = models.BooleanField()
+#     macrame = models.BooleanField()
+#     rug_making = models.BooleanField()
+#     sewing = models.BooleanField()
+#     shoemaking = models.BooleanField()
+#     weaving = models.BooleanField()
+#     bookbinding = models.BooleanField()
+#     calligraphy = models.BooleanField()
+#     cardmaking = models.BooleanField()
+#     collage = models.BooleanField()
+#     drawing = models.BooleanField()
+#     origami = models.BooleanField()
+#     painting = models.BooleanField()
+#     papier_mache = models.BooleanField()
+#     quilling = models.BooleanField()
+#     scrapbooking = models.BooleanField()
+#     stamping = models.BooleanField()
 
 class Image(models.Model):
     name = models.CharField(max_length=255)
@@ -31,9 +59,24 @@ class Resource(models.Model):
         on_delete=models.CASCADE,
         related_name='resources',
     )
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
     link = models.URLField(max_length=400)
-    media_type = models.CharField(max_length=100)
+    media_type = models.CharField(max_length=255)
+    notes = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Material(models.Model):
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='materials',
+    )
+    name = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255)
+    categories = models.CharField(max_length=255)
+    material_purchase_link = models.URLField(max_length=400)
     notes = models.TextField()
 
     def __str__(self):
