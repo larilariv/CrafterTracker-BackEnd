fbb199052fcd067e1942c11fdf4375361da7d64a
Larissa Vieira <lv23@nyu.edu>
2022-10-08 23:58:30 -0400

    Added notes textfield to Projects model

--- crafter_tracker/models.py
+++ crafter_tracker/models.py
@@ -16,11 +16,11 @@ class Project(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     # category = ArrayField(models.CharField(max_length=250))
+    notes = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
     # create_date = models.DateField(auto_now_add=True)
     # start_date = models.DateField(null=True, blank=True)
     # end_date = models.DateField(null=True, blank=True)
-    # notes = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
